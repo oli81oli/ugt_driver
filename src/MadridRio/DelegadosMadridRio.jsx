7d8ba1f1de1f1b9d92cbf64102f237df8e30f07b
oli81oli <oli.b.alonso@gmail.com>
2026-04-30 17:20:28 +0200

    fixed issues

--- src/MadridRio/DelegadosMadridRio.jsx
+++ src/MadridRio/DelegadosMadridRio.jsx
@@ -10,10 +10,10 @@ export const DelegadosMadridRio = ({ name, showInfoDelegados, setShowFooterChat 
             <TextContact name={name} />
             <div className='profile-container'>
                 <ContactBox props={{ name: 'Marco', status: 'de Comité', src: 'https://res.cloudinary.com/dpflfpgcj/image/upload/v1701871263/uploads/v8xummfbqaofoq3w7jou.png', phone: '34604226991' }} />
-                <ContactBox props={{ name: 'Daniel', status: 'de Comité', src: 'https://res.cloudinary.com/dpflfpgcj/image/upload/v1777557066/kwr95i7pkb7lwlpfat62.jpg', phone: '34609104030' }} />
+                <ContactBox props={{ name: 'Daniel', status: 'de Comité', src: 'https://res.cloudinary.com/dpflfpgcj/image/upload/v1777562354/hlspihlk45kgtarddbzs.jpg', phone: '34609104030' }} />
                 <ContactBox props={{ name: 'Gustavo', status: 'de Comité', src: 'https://res.cloudinary.com/dpflfpgcj/image/upload/v1701871263/uploads/v8xummfbqaofoq3w7jou.png', phone: '34600083159' }} />
-                <ContactBox props={{ name: 'Consuelo', status: 'de Comité', src: 'https://res.cloudinary.com/dpflfpgcj/image/upload/v1777557056/r3mk0aryo4mbmeq9qjg1.jpg', phone: '34633241085' }} />
-                <ContactBox props={{ name: 'Felipe', status: 'de Comité', src: 'https://res.cloudinary.com/dpflfpgcj/image/upload/v1777557073/byjrh7ruhhrouxsrlvqa.jpg', phone: '34627561990' }} />
+                <ContactBox props={{ name: 'Consuelo', status: 'de Comité', src: 'https://res.cloudinary.com/dpflfpgcj/image/upload/v1777562330/qxfngrbebibfioijvvin.jpg', phone: '34633241085' }} />
+                <ContactBox props={{ name: 'Felipe', status: 'de Comité', src: 'https://res.cloudinary.com/dpflfpgcj/image/upload/v1777562377/zshkve0grjz4dkkr11ac.jpg', phone: '34627561990' }} />
                 <ContactBox props={{ name: 'Juan Gabriel', status: 'de Comité', src: 'https://res.cloudinary.com/dpflfpgcj/image/upload/v1701871263/uploads/v8xummfbqaofoq3w7jou.png', phone: '34603919801' }} />
                 <ContactBox props={{ name: 'Rafael', status: 'de Comité', src: 'https://res.cloudinary.com/dpflfpgcj/image/upload/v1701871263/uploads/v8xummfbqaofoq3w7jou.png', phone: '34657577798' }} />
             </div>
